5e9021770a01e2b54a7dba00170855f8f4119d4b
Vincent Bowen <vibo3564@colorado.edu>
2024-02-16 17:11:08 -0700

    formatting board

--- play.py
+++ play.py
@@ -1,11 +1,12 @@
 from src.python.bot.mancala_bot import Bot as bot
 from src.python.board.mancala_board import Board as mancala
+from src.python.moves import random_moves
 
 game = mancala()
 bot = bot(3, game, 2)
 while not game.game_over:
     bot.reset(3, game, 1)
     bot.best_move(game, True)
-    game.random_move_generator()
+    random_moves.random_move_generator(game)
     game.render_board()
 print(game.winner)
