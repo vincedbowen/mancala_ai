53f31f61b1f29d0b17baf06af40e04769d2c9a1c
Vincent Bowen <vibo3564@colorado.edu>
2024-02-17 00:28:48 -0700

    making the board have more useful functions to determine states and turns

--- board/new_board.py
+++ board/new_board.py
@@ -4,6 +4,15 @@ import math
 class Board:
     def __init__(self, pits_per_player=6, stones_per_pit=4):
         # Constructor calls reset so we can run this through a loop without persistence
+        self.winner = None
+        self.game_over = None
+        self.total_pits = None
+        self.pits_index = None
+        self.moves = None
+        self.current_player = None
+        self.players = None
+        self.virtual_board = None
+        self.pits_per_player = None
         self.reset(pits_per_player, stones_per_pit)
 
     def reset(self, pits_per_player, stones_per_pit):
@@ -44,7 +53,7 @@ class Board:
             if pit < 1 or pit > self.pits_per_player:
                 return False
         else:
-            if pit < self.pits_per_player + 1 or pit > 2 * self.pits_index[1]:
+            if pit < self.pits_per_player + 1 or pit > 2 * self.pits_per_player:
                 return False
         if self.virtual_board[pit] == 0:
             return False
@@ -61,129 +70,18 @@ class Board:
         """
         return pit + self.pits_per_player
 
-    def play(self, pit):
+    def move(self, pit):
         """
-        This function simulates a single move made by a specific player using their selected pit. It primarily performs three tasks:
-        1. It checks if the chosen pit is a valid move for the current player. If not, it prints "INVALID MOVE" and takes no action.
-        2. It verifies if the game board has already reached a winning state. If so, it prints "GAME OVER" and takes no further action.
-        3. After passing the above two checks, it proceeds to distribute the stones according to the specified Mancala rules.
 
-        Finally, the function then switches the current player, allowing the other player to take their turn.
+        :param pit:
+        :return:
         """
-        win = self.winning_eval()
-        if win[0]:
-            pass
-        elif not self.valid_move(pit):
-            print("Invalid Move! Please try another Move.")
-            pass
+        if self.winning_eval():
+            self.game_over = True
+        elif self.valid_move(pit):
+            print("Invalid Move!")
         else:
-            move = self.current_player, " selects ", pit
-            self.moves.append(move)
-            if self.current_player == 1:
-                # empty the selected pit
-                num_stones = self.player_one_side[pit]
-                self.player_one_side[pit] = 0
-                # distribute the stones towards the player's mancala
-                initial_dist = math.floor((num_stones / self.total_pits))
-                for key in self.player_one_side:
-                    # Dictionaries have the same keys so we can use one for loop to increment everything
-                    self.player_one_side[key] += initial_dist
-                    self.player_two_side[key] += initial_dist
-                # Number of leftover stones that need to go around less than one full time
-                leftovers = num_stones % self.total_pits
-                # increment or decrement the pit we are depositing into
-                moving_pit = pit
-                side = self.current_player
-                # Check if mancala deposit was last play
-                last_played = False
-                while leftovers > 0:
-                    if side == 1:
-                        moving_pit -= 1
-                    else:
-                        moving_pit += 1
-                    if moving_pit < 1 and side == 1:
-                        self.player_one_side['mancala'] += 1
-                        side = 2
-                        leftovers -= 1
-                        moving_pit = 0
-                        last_played = True
-                    elif moving_pit > 6 and side == 2:
-                        self.player_two_side['mancala'] += 1
-                        side = 1
-                        leftovers -= 1
-                        moving_pit = 7
-                        last_played = True
-                    else:
-                        if side == 1:
-                            self.player_one_side[moving_pit] += 1
-                        else:
-                            self.player_two_side[moving_pit] += 1
-                        leftovers -= 1
-                        last_played = False
-                # Check if the last play was an empty slot own that player's side and steal all the oppposites
-                if last_played == False and self.player_one_side[moving_pit] == 1 and side == 1:
-                    self.player_one_side['mancala'] += self.player_two_side[moving_pit]
-                    self.player_two_side[moving_pit] = 0
-                    self.player_one_side['mancala'] += 1
-                    self.player_one_side[moving_pit] = 0
-                print(side)
-                if moving_pit == 0 and side == 2:
-                    pass
-                else:
-                    # change current player
-                    self.current_player = 2
-            else:
-                # empty the selected pit
-                num_stones = self.player_two_side[pit]
-                self.player_two_side[pit] = 0
-                # distribute the stones towards the player's mancala
-                initial_dist = math.floor((num_stones / self.total_pits))
-                for key in self.player_two_side:
-                    # Dictionaries have the same keys so we can use one for loop to increment everything
-                    self.player_one_side[key] += initial_dist
-                    self.player_two_side[key] += initial_dist
-                # Number of leftover stones that need to go around less than one full time
-                leftovers = num_stones % self.total_pits
-                # increment or decrement the pit we are depositing into
-                moving_pit = pit
-                side = self.current_player
-                # Check if mancala deposit was last play
-                last_played = False
-                while (leftovers > 0):
-                    if (side == 1):
-                        moving_pit -= 1
-                    else:
-                        moving_pit += 1
-                    if (moving_pit < 1 and side == 1):
-                        self.player_one_side['mancala'] += 1
-                        side = 2
-                        leftovers -= 1
-                        moving_pit = 0
-                        last_played = True
-                    elif (moving_pit > 6 and side == 2):
-                        self.player_two_side['mancala'] += 1
-                        side = 1
-                        leftovers -= 1
-                        moving_pit = 7
-                        last_played = True
-                    else:
-                        if (side == 1):
-                            self.player_one_side[moving_pit] += 1
-                        else:
-                            self.player_two_side[moving_pit] += 1
-                        leftovers -= 1
-                        last_played = False
-                # Check if the last play was an empty slot own that player's side and steal all the oppposites
-                if (last_played == False and self.player_two_side[moving_pit] == 1 and side == 2):
-                    self.player_two_side['mancala'] += self.player_one_side[moving_pit]
-                    self.player_two_side['mancala'] += 1
-                    self.player_two_side[moving_pit] = 0
-                    self.player_one_side[moving_pit] = 0
-                if moving_pit == 0 and side == 2:
-                    pass
-                else:
-                    # change current player
-                    self.current_player = 1
+            
 
     def winning_eval(self):
         """
@@ -197,17 +95,51 @@ class Board:
                     game_over = False
         return game_over
 
-    def check_for_capture(self):
-        pass
+    def can_capture(self, current_player, ending_pit):
+        """
+        Checks if the last pit a stone was dropped in is on the player's side. If so, the player can capture the stones
+        from opponent's pit across the board
+        :param current_player: The player whose turn it currently is
+        :param ending_pit: The last pit a stone was dropped as a dictionary key
+        """
+        if current_player == 1 and 1 <= ending_pit <= self.pits_per_player:
+            if self.virtual_board[ending_pit] == 1:
+                self.capture(ending_pit)
+        elif current_player == 2 and self.pits_per_player + 1 <= ending_pit <= 2 * self.pits_per_player:
+            if self.virtual_board[ending_pit] == 1:
+                self.capture(ending_pit)
+
+    def capture(self, ending_pit):
+        """
+        Actually captures the stone from the opponent's opposite pit
+        :param ending_pit: The pit in which to the current opponent ended on
+        """
+        opposite = (2 * self.pits_per_player + 1) - ending_pit
+        self.virtual_board[ending_pit] += opposite
+        self.virtual_board[opposite] = 0
+
+    def check_for_play_again(self, current_player, ending_pit):
+        """
+        Determines if the player dropped the last stone in their own store. In that case, the player gets to play again
+        :param current_player: The player whose turn it currently is
+        :param ending_pit: The last pit a stone was dropped in as a dictionary key
+        :return: A boolean value representing whether the player should play again
+        """
+        if current_player == 1 and ending_pit == 'player 1 mancala':
+            return True
+        elif current_player == 2 and ending_pit == 'player 2 mancala':
+            return True
+        else:
+            return False
 
     def determine_winner(self):
         """
         Determines who won the game
         :return: The winner of the game. If the game is a tie, return None
         """
-        if(self.virtual_board['player 1 mancala'] > self.virtual_board['player 2 mancala']):
+        if (self.virtual_board['player 1 mancala'] > self.virtual_board['player 2 mancala']):
             return 1
-        elif(self.virtual_board['player 2 mancala'] > self.virtual_board['player 1 mancala']):
+        elif (self.virtual_board['player 2 mancala'] > self.virtual_board['player 1 mancala']):
             return 2
         else:
             return None
@@ -216,8 +148,7 @@ class Board:
         self.current_player = 3 - self.current_player
 
     def render_board(self):
-        print(self.player_one_side)
-        print("              ", self.player_two_side)
+        print(self.virtual_board)
 
 
 board = Board()
