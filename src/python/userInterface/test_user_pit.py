5da6a92624b8296fead17c371c761d2732b8aaaf
Vincent Bowen <vibo3564@colorado.edu>
2024-02-16 18:41:39 -0700

    fixing file format

--- src/python/userInterface/test_user_pit.py
+++ src/python/userInterface/test_user_pit.py
@@ -2,6 +2,7 @@ from unittest import mock
 from unittest import TestCase
 import user_pit
 
+
 class Test(TestCase):
     @mock.patch('user_pit.input', create=True)
     def test_get_pit(self, mock_input):
